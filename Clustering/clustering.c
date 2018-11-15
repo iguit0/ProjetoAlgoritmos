c40a7cc8d0d7cf580a0fb7de6f8368af5f7a8cb4
Igor Alves <igorsk89@gmail.com>
2018-11-15 02:43:40 -0200

    Correções

--- Clustering/clustering.c
+++ Clustering/clustering.c
@@ -1,120 +1,134 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Estrutura para modularizar o 'freekick' da célula
 typedef struct sTipoItem {
-    int vertice; // vertice destino (dest)
+    int vertice; // vertice destino
     double peso;
 } ITEM;
 
-// Estrutura para representar uma celula da lista de adjancecia
 typedef struct sCell {
     ITEM info;
     struct sCell* prox;
 } CELULA;
 
-// Estrutura para representar uma lista de adjacencia
 typedef struct sLista {
     CELULA* primeiro;
     CELULA* ultimo;
 } LISTA;
 
-// Estrutura para representar um grafo
-// Um grafo é um array de lista de adjacencias
-// O tamanho do grafo vai ser V (numero de vertices no grafo)
 typedef struct sGrafo {
-    LISTA* adj; // lista de adjacência
+    LISTA adj;
     int numVertices;
     int numArestas;
 } GRAFO;
 
-CELULA* criarCelula(int dest);
-GRAFO* criaGrafo(int numVertices);
-void inserirAresta(GRAFO* grafo,int u,int v,double peso);
-void imprimeGrafo(GRAFO* grafo);
+GRAFO* criaGrafo(int numVertices, int numArestas) {
+    int i;
+    GRAFO* g = (GRAFO*) malloc(numVertices*sizeof(GRAFO));
+    if(g == NULL){
+        printf("\nErro ao alocar grafo!");
+        return NULL;
+    }
+    g->numArestas = numArestas;
+    g->numVertices = numVertices;
+    for(i=0;i<g->numVertices;i++){
+        g[i].adj.primeiro = NULL;
+        g[i].adj.ultimo = NULL;
+    }
+    return g;
+}
+
+int existeAresta(LISTA* lista, int v){
+    CELULA *aux = lista->primeiro;
+    int existe = 0;
+	if(aux == NULL){
+        return 0;
+    }
+	while(aux != NULL){
+        if(aux->info.vertice == v){
+            existe = 1;
+			break;
+        }
+        aux = aux->prox;
+    }
+	return existe;
+}
+
+int inserir(LISTA* lista,int v,double w){
+    if(existeAresta(lista,v)){
+        printf("A aresta ja existe!\n");
+        return 0;
+    }
+    CELULA* p = (CELULA *) malloc(sizeof(CELULA));
+    if(p==NULL){
+        printf("Nao foi possivel alocar!\n");
+        return 0;
+    }
+    (p->info).vertice = v;
+    (p->info).peso = w;
+    p->prox = NULL;
+    if(lista->primeiro == NULL){
+        lista->primeiro = p;
+        lista->ultimo = p;
+        return 1;
+    }
+    (lista->ultimo)->prox = p;
+    lista->ultimo = p;
+    return 1;
+}
+
+void inserirAresta(GRAFO* g,int u,int v,double w){
+    if(g==NULL){
+        return;
+    }
+    if(inserir(&g[u].adj,v,w)){
+        g->numArestas++;
+    }
+}
+
+void imprimirGrafo(GRAFO* grafo){
+    if(grafo == NULL){
+	   printf("\n Grafo Vazio!\n");
+       return;
+	}else{
+		int i;
+		for(i = 0; i < grafo->numVertices; i++){
+		    CELULA* aux = grafo[i].adj.primeiro;
+			printf("Vertice: %d --> ", i);
+			while(aux != NULL){
+			    printf(" %d(%.2lf)", aux->info.vertice,aux->info.peso);
+				aux = aux->prox;
+			}
+			    printf("\n");
+	    }
+	}
+}
 
 int main() {
-    GRAFO* gr = criaGrafo(10);
+    GRAFO* gr = criaGrafo(3,9);
     FILE* f;
-    int i,u,v;
+    int u,v;
     double w;
     f = fopen("entrada-teste.txt","r");
     if(f == NULL){
         printf("Erro na abertura do arquivo!\n");
         exit(1);
     }
-
-    while(fscanf(f,"%d %d %lf",&u,&v,&w) != EOF){
-        //printf("%d %d %.2lf\n",u,v,w);
+    int i;
+    printf("\tG(%d,%d)\n",gr->numVertices,gr->numArestas);
+    while(fscanf(f,"%d %d %lf",&u,&v,&w)!=EOF){
         inserirAresta(gr,u,v,w);
     }
-
-    imprimeGrafo(gr);
+    //outra forma de ler o arquivo
+    /*for(i=0;i<9;i++){
+        fscanf(f,"%d %d %lf",&u,&v,&w);
+        //printf("%d %d %.2lf\n",u,v,w);
+        inserirAresta(gr,u,v,w);
+    }*/
+    imprimirGrafo(gr);
 
     fclose(f);
     free(gr);
+    gr = NULL;
     return 0;
-}
-
-// Criar nova celula para inserir na lista de adjacencia
-CELULA* criarCelula(int dest) {
-    CELULA* novo = (CELULA*) malloc(sizeof(CELULA));
-    novo->info.vertice = dest;
-    novo->prox = NULL;
-    return novo;
-}
-
-// Funcao para criar o grafo com V vertices
-GRAFO* criaGrafo(int numVertices) {
-    GRAFO* g = (GRAFO*) malloc(sizeof(GRAFO));
-    if(g == NULL){
-        printf("\nErro ao alocar grafo!");
-        return NULL;
-    }
-    g->numVertices = numVertices;
-    g->adj = (LISTA*) malloc(numVertices*sizeof(LISTA));
-    int i;
-    for(i=0;i<numVertices;i++){
-        g->adj[i].primeiro = NULL;
-        g->adj[i].ultimo = NULL;
-    }
-    return g;
-}
-
-// Insere uma aresta no grafo
-void inserirAresta(GRAFO* grafo, int u,int v,double peso) {
-    // Insere uma aresta da raíz para destino
-    // Uma nova celula é inserida na lista de adjacencia de raízes
-    // a celula é inserida no inicio
-    if(grafo == NULL) return;
-    CELULA* novo = criarCelula(v);
-    if(novo==NULL){
-        printf("\nNao foi possivel alocar!");
-        exit(1);
-    }
-    novo->info.peso = peso;
-    novo->prox = grafo->adj[u].primeiro;
-    grafo->adj[u].primeiro = novo;
-
-    // como o grafo nao é direcionado, adiciona-se tambem
-    // uma aresta do destino para a raíz
-    novo = criarCelula(u);
-    novo->info.peso = peso;
-    novo->prox = grafo->adj[v].primeiro;
-    grafo->adj[v].primeiro = novo;
-}
-
-// Imprimir a lista de adjacencia do grafo
-// V[numero do vertice]: aresta(peso) ->
-void imprimeGrafo(GRAFO* g) {
-	int v;
-	for (v = 0; v < g->numVertices; ++v) {
-        CELULA* aux = g->adj[v].primeiro;
-		printf("V[%d]: ", v);
-		while (aux) {
-			printf("%d(%.2lf) -> ", aux->info.vertice,aux->info.peso);
-			aux = aux->prox;
-		}
-		printf("\n");
-	}
 }
